a696c40f3c0db03d41dc5963109f342ef1327c16
Manel <manel.castro39@gmail.com>
2020-09-14 14:45:41 +0200

    delete button

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -5,3 +5,7 @@ export const CREATE_COURSE_SUCCESS = "CREATE_COURSE_SUCCESS";
 export const UPDATE_COURSE_SUCCESS = "UPDATE_COURSE_SUCCESS";
 export const BEGIN_API_CALL = "BEGIN_API_CALL";
 export const API_CALL_ERROR = "API_CALL_ERROR";
+
+//Optimistic means that we're updating the state before updating the API.
+//We need to end the constant with _OPTIMISTIC in order to don't change the Spinner state.
+export const DELETE_COURSE_OPTIMISTIC = "DELETE_COURSE_OPTIMISTIC";
